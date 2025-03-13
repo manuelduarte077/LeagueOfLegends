ae692f406cd5e58c8c537d28ca2dbdcca8eeddd8
manuelduarte077 <carlosd99m@gmail.com>
2025-03-13 02:56:20 -0600

    Refactor: Implement ApiRepository interface and add Champion data models

--- app/src/main/java/dev/donmanuel/kotlinandroidtemplate/domain/repository/ApiRepository.kt
+++ app/src/main/java/dev/donmanuel/kotlinandroidtemplate/domain/repository/ApiRepository.kt
@@ -1,5 +1,11 @@
 package dev.donmanuel.kotlinandroidtemplate.domain.repository
 
+import com.skydoves.sandwich.ApiResponse
+import dev.donmanuel.kotlinandroidtemplate.domain.model.ChampionResponseModel
+
 interface ApiRepository {
 
+    suspend fun getAllChampions(): ApiResponse<ChampionResponseModel>
+
+    suspend fun getChampionByName(name: String): ApiResponse<ChampionResponseModel>
 }
